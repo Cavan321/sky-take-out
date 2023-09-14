b8372a51b48fd1a6bb0b2942d45ea449b462541f
cavan <cavanhht@163.com>
2023-09-14 16:15:23 +0800

    新增员工功能开发

--- sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -15,13 +18,32 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
+     *
      * @param ex
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /*
+     *@Description: 处理SQL异常
+     *@Param: ex
+     *@Author: Cavan
+     *@Date: 2023/9/14
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        //Duplicate entry 'houjieshu' for key 'employee.idx_username'
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")) {
+            String[] s = message.split(" ");
+            String username = s[2];
+            return Result.error(username + MessageConstant.ALREADY_EXISTS);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
     }
 
 }
