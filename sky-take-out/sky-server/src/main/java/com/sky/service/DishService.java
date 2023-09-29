7a08ec43765f694070ff287ae453699f11816acc
cavan <cavanhht@163.com>
2023-09-29 18:35:10 +0800

    套餐管理：新增套餐、套餐分页查询、删除套餐、修改套餐、起售停售套餐功能的实现

--- sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 
@@ -55,4 +56,18 @@ public interface DishService {
     * @return void
     */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish:
+     * @return List<DishVO>
+     */
+    List<DishVO> listWithFlavor(Dish dish);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId:
+     * @return List<Dish>
+     */
+    List<Dish> list(Long categoryId);
 }
