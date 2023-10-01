aa132219cde81943f39f41554c03cf9d5b3e2aff
cavan <cavanhht@163.com>
2023-10-01 18:56:52 +0800

    删除购物车中的一个商品

--- sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -24,4 +24,11 @@ public interface ShoppingCartService {
      * @return List<ShoppingCart>
      */
     List<ShoppingCart> showShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO:
+     * @return void
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
