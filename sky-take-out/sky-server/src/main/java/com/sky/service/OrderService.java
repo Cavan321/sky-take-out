44be460d18301b8f6a87532f80ae02d940e1f132
cavan <cavanhht@163.com>
2023-10-07 13:27:21 +0800

    用户催单业务

--- sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -112,4 +112,11 @@ public interface OrderService {
      * @return void
      */
     void complete(Long id);
+
+    /**
+     * 用户催单
+     * @param id:
+     * @return void
+     */
+    void reminder(Long id);
 }
